4a5c16cb72835e4d421a89ca7f6b3771255480da
T9404 <ghubman1@gmail.com>
2023-11-17 11:32:18 +0700

    Added base routing between registration and home pages

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,15 @@
+import {Route, Routes} from "react-router-dom"
+import RegistrationPage from "../pages/RegistrationPage/RegistrationPage";
+import HomePage from "../pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
+      <div className="container">
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/registration" element={<RegistrationPage />}/>
+          </Routes>
+      </div>
   );
 }
 
